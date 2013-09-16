6522c5563053fc13a896dfbfcaceca2d1a354537
UndeadScythes <daveyognaut2@gmail.com>
2013-09-16 15:09:02 +0100

    Remove TagType

--- src/main/java/com/undeadscythes/gedform/Tag.java
+++ src/main/java/com/undeadscythes/gedform/Tag.java
@@ -11,16 +11,4 @@ public interface Tag {
      * @return The literal representation of a {@link Tag}
      */
     String getTag();
-
-    /**
-     *
-     * @return A user friendly description of this tag
-     */
-    String getFriendly();
-
-    /**
-     *
-     * @return The type of data this tag pertains to
-     */
-    TagType getType();
 }
