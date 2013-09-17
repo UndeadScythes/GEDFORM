36995025b3015b3b3c146b2c547df80a18999f5e
UndeadScythes <daveyognaut2@gmail.com>
2013-09-17 11:18:04 +0100

    Finalize structures

--- src/main/java/com/undeadscythes/gedform/LineStruct.java
+++ src/main/java/com/undeadscythes/gedform/LineStruct.java
@@ -1,104 +1,94 @@
 package com.undeadscythes.gedform;
 
+import com.undeadscythes.gedform.exception.*;
+import static java.lang.Integer.*;
+import java.util.*;
+import org.apache.commons.lang3.*;
+
 /**
  * A {@link LineStruct} represents a single line of a GEDFORM record.
  *
  * @author UndeadScythes
  */
 public class LineStruct {
+    private static final String XREF_REGEX = "@\\w[^@\\x00-\\x1F\\x7F]{0,19}@";
+    private static final int MIN_ELEMENTS = 2;
+    private static final int MIN_XREF_ELEMENTS = 3;
+
+    private static String parseValue(final String[] strings, final int start) {
+        return StringUtils.join(Arrays.copyOfRange(strings, start, strings.length), " ").replace("@@", "@"); //TODO: Handle escape chars
+    }
+
+    private static String parseXREF(final String string) {
+        return string.replaceAll("@", "");
+    }
+
     /**
-     * Hierarchical level of this line
+     * Hierarchical level of this line.
      */
     public final int level;
+
     /**
-     * A relational link within the GEDFORM
+     * A relational link within the GEDFORM.
      */
     public final String xref;
+
     /**
-     * The {@link Tag} of this line
+     * The {@link LineStruct#tag} of this line.
      */
-    public final Tag tag;
+    public final String tag;
+
     /**
-     * This line's value
+     * This line's value.
      */
     public final String value;
+
     /**
-     * Whether this {@link LineStruct#value} is a pointer
+     * True if this {@link LineStruct#value} is a pointer.
      */
     public final boolean pointer;
 
     /**
+     * Takes a single line of transmission and divides it into its constituent
+     * parts.
      *
-     * @param tag
+     * @param string Line of transmission
+     * @throws ParsingException When a parsing error occurs
      */
-    public LineStruct(final Tag tag) {
-        this(0, tag);
-    }
-
-    /**
-     *
-     * @param level
-     * @param tag
-     */
-    public LineStruct(final int level, final Tag tag) {
-        this(level, tag, "");
-    }
-
-    /**
-     *
-     * @param level
-     * @param tag
-     * @param value
-     */
-    public LineStruct(final int level, final Tag tag, final String value) {
-        this(level, tag, value, false);
-    }
-
-    /**
-     *
-     * @param level
-     * @param tag
-     * @param value
-     * @param pointer
-     */
-    public LineStruct(final int level, final Tag tag, final String value, final boolean pointer) {
-        this(level, "", tag, value, pointer);
-    }
-
-    /**
-     *
-     * @param level
-     * @param xref
-     * @param tag
-     */
-    public LineStruct(final int level, final String xref, final Tag tag) {
-        this(level, xref, tag, "");
-    }
-
-    /**
-     *
-     * @param level
-     * @param xref
-     * @param tag
-     * @param value
-     */
-    public LineStruct(final int level, final String xref, final Tag tag, final String value) {
-        this(level, xref, tag, value, false);
-    }
-
-    /**
-     *
-     * @param level
-     * @param xref
-     * @param tag
-     * @param value
-     * @param pointer
-     */
-    public LineStruct(final int level, final String xref, final Tag tag, final String value, final boolean pointer) {
-        this.level = level;
-        this.xref = xref;
-        this.tag = tag;
-        this.value = value;
-        this.pointer = pointer;
+    public LineStruct(final String string) throws ParsingException {
+        final String[] split = string.split(" ");
+        try {
+            level = parseInt(split[0]);
+        } catch (NumberFormatException ex) {
+            throw new ParsingException("Could not parse level.", ex);
+        }
+        if (split.length < MIN_ELEMENTS) {
+            throw new ParsingException("Not enough elements.");
+        } else if (split.length == MIN_ELEMENTS) { // LVL TAG
+            tag = split[1];
+            xref = "";
+            value = "";
+            pointer = false;
+        } else if (split[1].matches(XREF_REGEX)) {
+            xref = parseXREF(split[1]);
+            tag = split[2];
+            if (split.length == MIN_XREF_ELEMENTS) { // LVL XREF TAG
+                value = "";
+            } else { // LVL XREF TAG VAL
+                value = parseValue(split, 3);
+            }
+            pointer = false;
+        } else {
+            xref = "";
+            tag = split[1];
+            final String temp = parseValue(split, 2);
+            if (temp.matches(XREF_REGEX)) { // LVL TAG PTR
+                value = parseXREF(temp);
+                pointer = true;
+            } else { // LVL TAG VAL
+                value = temp;
+                pointer = false;
+            }
+        }
     }
 }
--- src/main/java/com/undeadscythes/gedform/Transmission.java
+++ src/main/java/com/undeadscythes/gedform/Transmission.java
@@ -1,124 +1,119 @@
 package com.undeadscythes.gedform;
 
+import com.undeadscythes.betterreader.*;
 import com.undeadscythes.gedform.exception.*;
 import java.io.*;
-import static java.lang.Integer.*;
-import static java.util.Arrays.*;
 import java.util.*;
-import static org.apache.commons.lang3.StringUtils.*;
 
 /**
  * A {@link Transmission} is the basis for GEDFORM communication.
  *
  * @author UndeadScythes
  */
-public class Transmission extends ArrayList<Record> {
+public class Transmission extends ArrayList<Cluster> {
     private static final long serialVersionUID = 1L;
-    private static final String XREF_REGEX = "@\\w[^@\\x00-\\x1F\\x7F]{0,19}@";
 
     /**
+     * Parse a file into a {@link Transmission}.
      *
      * @param file
-     * @param tags
-     * @return Pair containing parsed transmission and a set of unused tags
-     * @throws FileNotFoundException
-     * @throws IOException
+     * @return Parsed {@link Transmission}
+     * @throws ParsingException When a parsing error occurs
      */
-    public static Map.Entry<Transmission, Set<String>> parse(final File file, final Tag[] tags) throws FileNotFoundException, IOException {
-        final BufferedReader input = new BufferedReader(new FileReader(file));
+    public static Transmission parse(final File file) throws ParsingException {
+        final BetterReader reader;
+        try {
+            reader = new BetterReader(file);
+        } catch (FileNotFoundException ex) {
+            throw new ParsingException("Could not find specified file.", ex);
+        }
         final Transmission transmission = new Transmission();
-        Record record = new Record();
-        final Set<String> unused = new HashSet<String>(0);
-        String line;
-        while ((line = input.readLine()) != null) {
-            final String[] split = line.trim().split(" ");
-            final int level = parseInt(split[0]); //TODO: Catch integer parse exceptions
-            if (level == 0 && !record.isEmpty()) {
-                transmission.add(record);
-                record = new Record();
-            }
+        Cluster cluster = new Cluster();
+        while (reader.hasNext()) {
+            final String line = reader.getLine();
+            final LineStruct struct;
             try {
-                if (split.length < 2) {
-                    //TODO: Throw parse error
-                } else if (split.length == 2) {
-                    record.add(new LineStruct(level, getTag(tags, split[1]))); // LVL TAG
-                } else if (split[1].matches(XREF_REGEX)) {
-                    final String xref = split[1].replaceAll("@", "");
-                    final Tag tag = getTag(tags, split[2]);
-                    if (split.length == 3) {
-                        record.add(new LineStruct(level, xref, tag)); // LVL XREF TAG
-                    } else {
-                        final String value = join(copyOfRange(split, 3, split.length), " ").replace("@@", "@"); //TODO: Handle escape chars
-                        record.add(new LineStruct(level, xref, tag, value)); // LVL XREF TAG VAL
-                    }
-                } else {
-                    final Tag tag = getTag(tags, split[1]);
-                    final String value = join(copyOfRange(split, 3, split.length), " ").replace("@@", "@"); //TODO: Handle escape chars
-                    if (value.matches(XREF_REGEX)) {
-
-                        record.add(new LineStruct(level, tag, value.replaceAll("@", ""), true)); // LVL TAG PTR
-                    } else {
-                        record.add(new LineStruct(level, tag, value)); // LVL TAG VAL
-                    }
-                }
-            } catch (NoValidTagException ex) {
-                unused.add(ex.tag);
+                struct = new LineStruct(line);
+            } catch (ParsingException ex) {
+                throw new ParsingException("Could not parse line #" + reader.getLineNo() + ".", ex);
             }
+            if (struct.level == 0 && !cluster.isEmpty()) {
+                transmission.add(cluster);
+                cluster = new Cluster();
+            }
+            cluster.add(struct);
         }
-        return new AbstractMap.SimpleEntry<Transmission, Set<String>>(transmission, unused);
-    }
-
-    private static Tag getTag(final Tag[] tags, final String tag) {
-        for (final Tag test : tags) {
-            if (test.getTag().equals(tag)) return test;
-        }
-        throw new NoValidTagException(tag);
+        transmission.add(cluster);
+        return transmission;
     }
 
     private int index = 0;
 
     /**
-     *
+     * Blank {@link Transmission}.
      */
     public Transmission() {
         this(0);
     }
 
     /**
+     * {@link Transmission} with a given initial capacity.
      *
-     * @param size
+     * @param size Initial capacity
      */
     public Transmission(final int size) {
         super(size);
     }
 
     /**
+     * Copy this {@link Transmission}.
      *
-     * @return An copy of this {@link Transmission}
+     * @return Mutable copy of this {@link Transmission}
      */
     public Transmission copy() {
         final Transmission copy = new Transmission(this.size());
-        for (Record record : this) {
-            copy.add(record.copy());
+        for (Cluster cluster : this) {
+            copy.add(cluster.copy());
         }
         return copy;
     }
 
     /**
+     * Grabs the next {@link Cluster} in this {@link Transmission}.
      *
-     * @return The next {@link Record}
+     * @return The next {@link Cluster}
      */
-    public Record pullRecord() {
-        final Record record = get(index);
+    public Cluster pullCluster() {
+        final Cluster cluster = get(index);
         index++;
-        return record;
+        return cluster;
     }
 
     /**
+     * Check if there is another {@link Cluster} to return in this
+     * {@link Transmission}.
      *
-     * @return True if there is another {@link Record} to pull
+     * @return True if there is another {@link Cluster} to pull
      */
     public boolean hasNext() {
         return size() > index;
+    }
+
+    /**
+     * YAMLize this {@link Transmission}.
+     *
+     * @return This {@link Transmission} in valid YAML form
+     */
+    public String yamlize() {
+        return ""; //TODO: Implement the yamlizer
+    }
+
+    /**
+     * Validate this {@link Transmission}.
+     *
+     * @return True if this {@link Transmission} has valid form
+     */
+    public boolean verify() {
+        return true; //TODO: Implement verifier
     }
 }
